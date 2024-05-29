a6b88db380577850052de146b4f3138bcaf3d80a
Bad-Goty <Goty929@gmail.com>
2024-05-28 22:16:24 -0600

    Hola

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'perfumeria_oriu_user',
+    host: 'dpg-cp6bs9u3e1ms73a8cfv0-a.oregon-postgres.render.com',
+    database: 'perfumeria_oriu',
+    password: 'WR2Ikz8jXkDiw97QlVLAEz070tyVGbtd',
+    port: 5432
+});
+
+app.get('/api/getData', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM usuarios'); // Cambia 'your_table' por tu tabla real
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Database error' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
